c891af6296b0bf7d1f1bda14b4e70bffe2e23f9c
Silvian Ranetescu <silvian.ranetescu@gmail.com>
2023-09-19 20:09:12 +0300

    redux-persist added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,31 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { phonebookReducer } from './phonebookSlice';
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, phonebookReducer);
 export const store = configureStore({
   reducer: {
-    phonebook: phonebookReducer,
+    phonebook: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+export let persistor = persistStore(store);
